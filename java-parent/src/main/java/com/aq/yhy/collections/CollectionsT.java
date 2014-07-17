0c41c171c174a14ffc1a25e102e1c3ad33e33cdb
haiyuan.yang <haiyuan.yang@dianping.com>
2014-07-17 18:35:08 +0800

    add

---
+++ java-parent/src/main/java/com/aq/yhy/collections/CollectionsT.java
@@ -0,0 +1,24 @@
+package com.aq.yhy.collections;
+
+import java.util.*;
+
+/**
+ * User: haiyuan.yang
+ * Date: 14-7-17
+ */
+public class CollectionsT {
+
+    public static void main(String[] args){
+        List<Integer> arrayList = new ArrayList<Integer>();
+        List<Integer> linkedList = new LinkedList<Integer>();
+
+        Set<Integer> hashSet = new HashSet<Integer>();
+        Set<Integer> treeSet = new TreeSet<Integer>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<Integer>();
+
+        Map<Integer,Integer> hashMap = new HashMap<Integer, Integer>();
+        Map<Integer,Integer> treeMap = new TreeMap<Integer, Integer>();
+    }
+
+
+}
---
+++ java-parent/src/main/java/com/aq/yhy/collections/ListT.java
@@ -0,0 +1,9 @@
+package com.aq.yhy.collections;
+
+/**
+ * User: haiyuan.yang
+ * Date: 14-7-17
+ */
+public class ListT {
+
+}
